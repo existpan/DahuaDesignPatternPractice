d6f1c8e1791ec77355fb6804367f1b063b2d293a
existpan <1468083746@qq.com>
2022-06-25 22:36:07 +0800

    first commit code

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,7 +10,34 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("请输入A：");
+            string parameterA = Console.ReadLine();
+            Console.WriteLine("请输入运算符:");
+            string operation = Console.ReadLine();
+            Console.WriteLine("请输入B：");
+            string parameterB = Console.ReadLine();
+            double result = 0;
+            switch (operation)
+            {
+                case "+":
+                    result = Convert.ToDouble(parameterA) + Convert.ToDouble(parameterB);
+                    break;
+                case "-":
+                    result = Convert.ToDouble(parameterA) - Convert.ToDouble(parameterB);
+                    break;
+                case "*":
+                    result = Convert.ToDouble(parameterA) * Convert.ToDouble(parameterB);
+                    break;
+                case "/":
+                    if (parameterB != "0")
+                    {
+                        result = Convert.ToDouble(parameterA) / Convert.ToDouble(parameterB);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine(result.ToString());
         }
     }
 }
