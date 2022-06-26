c8b3b60c49d58b125777cec2baa0ba70ea2b323c
yxpan@zs-tek.com <yxpan@zs-tek.com>
2022-06-26 17:35:18 +0800

    添加异常抛出。

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,35 +10,41 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("请输入数字A：");
-            string parameterA = Console.ReadLine();
-            Console.WriteLine("请输入运算符+、-、*、/:");
-            string operation = Console.ReadLine();
-            Console.WriteLine("请输入数字B：");
-            string parameterB = Console.ReadLine();
-            double result = 0;
-            switch (operation)
+            try
             {
-                case "+":
-                    result = Convert.ToDouble(parameterA) + Convert.ToDouble(parameterB);
-                    break;
-                case "-":
-                    result = Convert.ToDouble(parameterA) - Convert.ToDouble(parameterB);
-                    break;
-                case "*":
-                    result = Convert.ToDouble(parameterA) * Convert.ToDouble(parameterB);
-                    break;
-                case "/":
-                    if (parameterB != "0")
-                    {
-                        result = Convert.ToDouble(parameterA) / Convert.ToDouble(parameterB);
-                    }
-                    break;
-                default:
-                    break;
+                Console.WriteLine("请输入数字A：");
+                string parameterA = Console.ReadLine();
+                Console.WriteLine("请输入运算符+、-、*、/:");
+                string operation = Console.ReadLine();
+                Console.WriteLine("请输入数字B：");
+                string parameterB = Console.ReadLine();
+                double result = 0;
+                switch (operation)
+                {
+                    case "+":
+                        result = Convert.ToDouble(parameterA) + Convert.ToDouble(parameterB);
+                        break;
+                    case "-":
+                        result = Convert.ToDouble(parameterA) - Convert.ToDouble(parameterB);
+                        break;
+                    case "*":
+                        result = Convert.ToDouble(parameterA) * Convert.ToDouble(parameterB);
+                        break;
+                    case "/":
+                        if (parameterB != "0")
+                        {
+                            result = Convert.ToDouble(parameterA) / Convert.ToDouble(parameterB);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine(result.ToString());
             }
-            Console.WriteLine(result.ToString());
-
+            catch (Exception ex)
+            {
+                throw new Exception( ex.Message);
+            }
         }
     }
 }
