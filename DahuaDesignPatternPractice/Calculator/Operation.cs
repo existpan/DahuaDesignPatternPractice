a4b512bf5f556caa1235eddefe7a600000979a84
yuxian <38322720+existpan@users.noreply.github.com>
2022-06-27 23:01:56 +0800

    Add files via upload
    通过使用继承Operation的方式，灵活调用不同派生类的方法，以实现继承与多态，增加程序灵活性；通过使用简单工厂的方式灵活创建对象。

---
+++ DahuaDesignPatternPractice/Calculator/Operation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    /// <summary>
+    /// 运算类
+    /// </summary>
+    public class Operation
+    {
+        public static double GetOperationResult(double parameterA, double parameterB, string operation)
+        {
+            double result = 0;
+            switch (operation)
+            {
+                case "+":
+                    result = parameterA + parameterB;
+                    break;
+                case "-":
+                    result = parameterA + parameterB;
+                    break;
+                case "*":
+                    result = parameterA + parameterB;
+                    break;
+                case "/":
+                    if (parameterB != 0)
+                    {
+                        result = parameterA / parameterB;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
---
+++ DahuaDesignPatternPractice/Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("请输入数字A：");
+                string parameterA = Console.ReadLine();
+                Console.WriteLine("请输入运算符+、-、*、/:");
+                string operation = Console.ReadLine();
+                Console.WriteLine("请输入数字B：");
+                string parameterB = Console.ReadLine();
+                double result = Operation.GetOperationResult(Convert.ToDouble(parameterA), Convert.ToDouble(parameterB), operation);
+                Console.WriteLine("\n结果是:"+result);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception( ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
